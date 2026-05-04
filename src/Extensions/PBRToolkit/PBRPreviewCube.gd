086e5eff9ae661823d1bd3279591934380a58e9e
Anti-Matt-Er <m.burrows@live.co.uk>
2026-05-04 23:07:19 +0100

    Create custom cube mesh for minecraft-style UV mapping

--- src/Extensions/PBRToolkit/PBRPreviewCube.gd
+++ src/Extensions/PBRToolkit/PBRPreviewCube.gd
@@ -1,8 +1,9 @@
+@tool
 class_name PBRPreviewCube
 extends MeshInstance3D
 
 
-@onready var material: StandardMaterial3D = mesh.material
+@onready var material: StandardMaterial3D = material_override
 @onready var albedo := ImageTexture.new()
 @onready var metallic := ImageTexture.new()
 @onready var roughness := ImageTexture.new()
@@ -15,6 +16,10 @@ var default_normal: Image
 
 
 func _ready() -> void:
+	if Engine.is_editor_hint():
+		generate_mesh()
+		return
+	
 	var white_image = Image.create_empty(1, 1, false, Image.FORMAT_RGB8)
 	white_image.set_pixel(0, 0, Color.WHITE)
 	var black_image = Image.create_empty(1, 1, false, Image.FORMAT_RGB8)
@@ -42,3 +47,47 @@ func _ready() -> void:
 
 func check_alpha() -> void:
 	material.transparency = BaseMaterial3D.TRANSPARENCY_ALPHA if albedo.has_alpha() else BaseMaterial3D.TRANSPARENCY_DISABLED
+
+
+func generate_mesh() -> void:
+	if mesh:
+		return
+		
+	mesh = ArrayMesh.new()
+	
+	var surface = SurfaceTool.new()
+	surface.begin(Mesh.PRIMITIVE_TRIANGLES)
+	
+	var s = 0.5 # Extents for unit-size cube
+	
+	var faces = [
+		[Vector3.BACK,    Vector3(-s,  s,  s), Vector3( s,  s,  s), Vector3( s, -s,  s), Vector3(-s, -s,  s)], # Front
+		[Vector3.FORWARD, Vector3( s,  s, -s), Vector3(-s,  s, -s), Vector3(-s, -s, -s), Vector3( s, -s, -s)], # Back
+		[Vector3.UP,      Vector3(-s,  s, -s), Vector3( s,  s, -s), Vector3( s,  s,  s), Vector3(-s,  s,  s)], # Top
+		[Vector3.DOWN,    Vector3(-s, -s,  s), Vector3( s, -s,  s), Vector3( s, -s, -s), Vector3(-s, -s, -s)], # Bottom
+		[Vector3.RIGHT,   Vector3( s,  s,  s), Vector3( s,  s, -s), Vector3( s, -s, -s), Vector3( s, -s,  s)], # Right
+		[Vector3.LEFT,    Vector3(-s,  s, -s), Vector3(-s,  s,  s), Vector3(-s, -s,  s), Vector3(-s, -s, -s)], # Left
+	]
+	
+	var uvs = [Vector2(0, 0), Vector2(1, 0), Vector2(1, 1), Vector2(0, 1)]
+
+	for face in faces:
+		var face_normal = face[0]
+		surface.set_normal(face_normal)
+		
+		surface.set_uv(uvs[0])
+		surface.add_vertex(face[1])
+		surface.set_uv(uvs[1])
+		surface.add_vertex(face[2])
+		surface.set_uv(uvs[2])
+		surface.add_vertex(face[3])
+		
+		surface.set_uv(uvs[0])
+		surface.add_vertex(face[1])
+		surface.set_uv(uvs[2])
+		surface.add_vertex(face[3])
+		surface.set_uv(uvs[3])
+		surface.add_vertex(face[4])
+
+	surface.commit(mesh)
+	
